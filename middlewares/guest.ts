725b39ac12da075d6dc17e05a163caffe0d17d1d
Gonza Lopez <gonzalo@digitalf2f.com>
2023-10-18 04:21:46 -0300

    fix: :bug: fix bad access token

--- middlewares/guest.ts
+++ middlewares/guest.ts
@@ -1,16 +1,31 @@
 import { Request, Response, NextFunction } from "express";
+import { AppDataSource } from "../data-source";
+import { User } from "../models/User";
 
 // Custom middleware to allow access to unauthenticated users for specific routes.
-export default function guest(req: Request, res: Response, next: NextFunction) {
+export default async function guest(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
     // Check if the user is unauthenticated
     const token = req.headers.authorization;
+    const userRepository = AppDataSource.getRepository(User);
 
-    if (token) {
-        return res
-            .status(403)
-            .json({ message: "Access forbidden for authenticated users" });
+    if (!token) {
+        return next();
+    }
+
+    const user = await userRepository.findOneBy({
+        access_token: token,
+    });
+
+    if (!user) {
+        return next();
     }
 
     // If the user is unauthenticated, continue with the request.
-    next();
+    return res
+        .status(403)
+        .json({ message: "Access forbidden for authenticated users" });
 }
--- seeders/user.seeder.ts
+++ seeders/user.seeder.ts
@@ -1,5 +1,17 @@
 import * as bcrypt from "bcrypt";
+import { AppDataSource } from "../data-source";
+import { User } from "../models/User";
 
-(async function () {
-    console.log(await bcrypt.hash("password", 10));
-})();
+AppDataSource.initialize()
+    .then(async () => {
+        const user = new User();
+
+        user.name = "Admin";
+        user.email = "admin@example.com";
+        user.password = await bcrypt.hash("password", 10);
+        user.generateAccessToken();
+
+        await AppDataSource.manager.save(user);
+    })
+    .catch((error) => console.log(error))
+    .finally(() => AppDataSource.destroy());
